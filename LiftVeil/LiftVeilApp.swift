1f516e4989e7211128bf43d980a99607a8b69f18
Vikram Kriplaney <markiv@gmail.com>
2020-09-19 00:53:32 +0200

    Initial Commit

---
+++ LiftVeil/LiftVeilApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftVeilApp.swift
+//  LiftVeil
+//
+//  Created by Vikram Kriplaney on 19.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct LiftVeilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
